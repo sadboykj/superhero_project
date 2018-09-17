17cf43fe6cb3860be09219c705541a8e101630f3
KJ Wilson <yourboykjdev@gmail.com>
2018-09-17 15:07:12 -0700

    setting up classes

---
+++ superheroes.py
@@ -0,0 +1,30 @@
+import random
+
+class Ability:
+    def __init__(self, name, attack_strength):
+        # set ability name
+        self.name = ability_name
+        # set attack strength
+        self.attack_strength = attack_strength
+
+
+    def attack(self):
+        # return attack value
+        self.attack = attack
+
+    def update_attack(self, attack_strength):
+        # update attck values
+        attack_strength += 2
+
+class Hero:
+    def __init__(self, name):
+        # names
+
+    def add_ability(self, ability):
+        # add ability to ability listen
+
+    def attack(self):
+        # run attack() on every ability hero has
+
+if __name__ = "__main__":
+    print(__name__)
