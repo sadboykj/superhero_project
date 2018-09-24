78a54484355b8791e2f101b4ea362325c65dcecf
KJ Wilson <yourboykjdev@gmail.com>
2018-09-24 16:17:30 -0700

    did a lot of thinking today

--- superheroes.py
+++ superheroes.py
@@ -9,6 +9,18 @@ class Hero:
         #
         # ability list
         self.abilities = list()
+        # amor list
+        self.amors = list()
+        #
+        self.start_health = health
+        self.health = health
+        self.deaths = 0
+        self.kills = 0
+
+    # str returns string representation
+    # works like .format
+    def __str__(self):
+        return self.name
 
     def add_ability(self, ability):
         # add ability to ability list
@@ -28,6 +40,22 @@ class Hero:
         # prints number of abilities
         # print(len(self.abilities))
 
+    def defend(self):
+        defense = 0
+
+        if len(self.amors) == 0:
+            return 0
+
+        for power_of in self.armors:
+            # defense = defense + powerOf.defense
+            defense += power_of.defend()
+
+        if self.health == 0:
+            defense = 0
+            return defense
+        else:
+            return defense
+
 class Ability:
     def __init__(self, name, attack_strength):
         # set ability name
@@ -46,25 +74,54 @@ class Ability:
         high = self.attack_strength
         #
         # use random.randint(a,b) to select attack values
-        attack_power = random.randint(low, high)
+        power = random.randint(low, high)
         #
         # return attack value between 0 and the full attack
-        return (attack_power)
+        return (power)
 
     def update_attack(self, attack_strength):
         # update attck values
         self.attack_strength = attack_strength
 
-# class Weapon(Ability):
-#     def attack(self):
-#         # returns random value
-#         # attack power is inherited
-#         low = 0
-#         high = self.attack_strength()
-#         attack = random.randint(low, high)
-#         #
-#         print(attack)
-#
+class Weapon(Ability):
+    def attack(self):
+        # returns random value
+        # attack power is inherited
+        power = random.randint(0, self.attack_strength)
+        print(power)
+
+class Team:
+    def init(self, team_name):
+        self.name = team_name
+        self.heroes = list()
+
+    def add_hero(self, Hero):
+        # add hero object to list of heroes
+        self.heroes.append(Hero)
+
+
+    def remove_hero(self, name):
+        # remove hero from listen
+        # if hero isnt found return zero
+        # to remove all heroes:
+        # self.heroes.clear()
+        # or del self.heroes[:]
+        # .remove removes the first matching value
+        # del[] and .pop remove the index
+        # but .pop also returns it
+        
+
+
+
+    def find_hero(self, name):
+        # find and return hero from list
+        # if hero isnt found return zero
+
+    def view_all_heroes(self):
+        # print out all heroes
+        print self.heroes
+
+
 # class Amor:
 #     def __init__(self, name, defense):
 #         # name
@@ -74,26 +131,6 @@ class Ability:
 #
 #     def defend(self):
 #
-#
-# class Team:
-#     def init(self, team_name):
-#         self.name = team_name
-#         self.heroes = list()
-#
-#     def add_hero(self, Hero):
-#         # add hero object to list of heroes
-#
-#
-#     def remove_hero(self, name):
-#         # remove hero from listen
-#         # if hero isnt found return zero
-#
-#     def find_hero(self, name):
-#         # find and return hero from list
-#         # if hero isnt found return zero
-#
-#     def view_all_heroes(self):
-#         # print out all heroes
 
 if __name__ == "__main__":
     hero = Hero("Wonder Woman")
